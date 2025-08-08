9cef38cf66bde50cd9a7de27f5d17ad336cea372
sajivddin <sajivddin.websolutions@gmail.com>
2025-08-08 17:32:51 +0530

    change

--- lib/Screen/courseReview.page.dart
+++ lib/Screen/courseReview.page.dart
@@ -18,11 +18,41 @@ class _CourseReviewPageState extends State<CourseReviewPage> {
           Container(
             width: MediaQuery.of(context).size.width,
             height: 215.h,
-            decoration: BoxDecoration(color: Color(0xFF001E6C),
-            
-            
+            decoration: BoxDecoration(color: Color(0xFF001E6C)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 50.h),
+                Row(
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Container(
+                        margin: EdgeInsets.only(left: 20.w),
+                        width: 44.w,
+                        height: 44.h,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 6.w),
+                          child: Icon(
+                            Icons.arrow_back_ios,
+                            color: Color(0xFF262626),
+                            size: 20.sp,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 50.w),
+                  ],
+                ),
+              ],
             ),
-            child: Column(),),
+          ),
         ],
       ),
     );
