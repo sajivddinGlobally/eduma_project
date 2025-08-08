2fb3face7aa054777527f169499dcd958d0b60f2
sajivddin <sajivddin.websolutions@gmail.com>
2025-08-08 18:38:30 +0530

    change

--- lib/Screen/courseReview.page.dart
+++ lib/Screen/courseReview.page.dart
@@ -13,62 +13,279 @@ class _CourseReviewPageState extends State<CourseReviewPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      body: Stack(
+        alignment: Alignment.topCenter,
         children: [
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: 215.h,
-            decoration: BoxDecoration(color: Color(0xFF001E6C)),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: 50.h),
-                Row(
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 215.h,
+                decoration: BoxDecoration(color: Colors.amber),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    InkWell(
+                    SizedBox(width: 30.w),
+                    GestureDetector(
                       onTap: () {
                         Navigator.pop(context);
                       },
                       child: Container(
-                        margin: EdgeInsets.only(left: 20.w),
+                        height: 44.w,
                         width: 44.w,
-                        height: 44.h,
                         decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.white,
+                          color: Color.fromARGB(255, 255, 255, 255),
+                          borderRadius: BorderRadius.circular(500.r),
                         ),
-                        child: Padding(
-                          padding: EdgeInsets.only(left: 6.w),
+                        child: Center(
                           child: Icon(
                             Icons.arrow_back_ios,
-                            color: Color(0xFF262626),
-                            size: 20.sp,
+                            color: Color.fromARGB(255, 4, 0, 0),
+                            size: 15.w,
                           ),
                         ),
                       ),
                     ),
-                    SizedBox(width: 50.w),
+                    Spacer(),
                     Text(
-                      "Course Review",
+                      "College Review",
                       style: GoogleFonts.roboto(
-                        fontSize: 26.sp,
+                        fontSize: 18.w,
+                        color: Color.fromARGB(255, 255, 255, 255),
+                      ),
+                    ),
+                    Spacer(),
+                    SizedBox(width: 70.w),
+                  ],
+                ),
+              ),
+              SizedBox(height: 150.h),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                color: Colors.white,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 111.h),
+                    Text(
+                      "dgjkhfdz",
+                      style: GoogleFonts.roboto(
+                        color: Colors.black,
+                        fontSize: 18,
                         fontWeight: FontWeight.w600,
-                        color: Colors.white,
+                        letterSpacing: -0.95,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 100.w, right: 100.w),
+                      child: Text(
+                        "{snap.data.collageDescription}",
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.roboto(
+                          color: Colors.black,
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: -0.55,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 18.h),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: 26,
+                          decoration: BoxDecoration(
+                            color: Color(0xFFDEDDEC),
+                            borderRadius: BorderRadius.circular(40.r),
+                          ),
+                          child: Center(
+                            child: Padding(
+                              padding: EdgeInsets.only(left: 15, right: 15),
+                              child: Text(
+                                "snap.data.city",
+                                style: GoogleFonts.roboto(fontSize: 10.w),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 10.w),
+                        Container(
+                          height: 26,
+                          decoration: BoxDecoration(
+                            color: Color(0xFFDEDDEC),
+                            borderRadius: BorderRadius.circular(40.r),
+                          ),
+                          child: Center(
+                            child: Padding(
+                              padding: EdgeInsets.only(left: 15, right: 15),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    Icons.star,
+                                    color: Color(0xFF9088F1),
+                                    size: 16,
+                                  ),
+                                  SizedBox(width: 5.w),
+                                  Text(
+                                    "snap. Review",
+                                    style: GoogleFonts.roboto(fontSize: 10.w),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 10.w),
+                        Container(
+                          height: 26,
+                          decoration: BoxDecoration(
+                            color: Color(0xFFDEDDEC),
+                            borderRadius: BorderRadius.circular(40.r),
+                          ),
+                          child: Center(
+                            child: Padding(
+                              padding: EdgeInsets.only(left: 15, right: 15),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    "Seat intake",
+                                    style: GoogleFonts.roboto(fontSize: 10.w),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 15.h),
+                    Divider(color: Colors.grey.shade300, height: 2),
+                    SizedBox(height: 15.h),
+                    Padding(
+                      padding: EdgeInsets.only(left: 15.w),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Branches",
+                            style: GoogleFonts.roboto(
+                              color: const Color.fromARGB(255, 0, 0, 0),
+                              fontWeight: FontWeight.w600,
+                              fontSize: 15.w,
+                            ),
+                          ),
+                          Spacer(),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 50.h,
+                      width: MediaQuery.of(context).size.width,
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 8.0.w),
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 2,
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding: EdgeInsets.all(4.0.w),
+                              child: Container(
+                                height: 26,
+                                decoration: BoxDecoration(
+                                  color: Color(0xFFDEDDEC),
+                                  borderRadius: BorderRadius.circular(40.r),
+                                ),
+                                child: Center(
+                                  child: Padding(
+                                    padding: EdgeInsets.only(
+                                      left: 15,
+                                      right: 15,
+                                    ),
+                                    child: Text(
+                                      "departments[index]",
+                                      style: GoogleFonts.roboto(fontSize: 10.w),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 15.h),
+                    Divider(color: Colors.grey.shade300, height: 2),
+                    SizedBox(height: 15.h),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: 20.w,
+                        right: 20.w,
+                        top: 10.h,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Reviews & Testimonials",
+                                style: GoogleFonts.roboto(
+                                  color: const Color.fromARGB(255, 0, 0, 0),
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 15.w,
+                                ),
+                              ),
+                              Spacer(),
+                              GestureDetector(
+                                onTap: () {},
+                                child: Text(
+                                  "View All",
+                                  style: GoogleFonts.roboto(
+                                    fontSize: 11.w,
+                                    fontWeight: FontWeight.w600,
+                                    color: Color(0xFF9088F1),
+                                    textBaseline: TextBaseline.ideographic,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
                     ),
                   ],
                 ),
-                SizedBox(height: 30.h),
-                Container(
-                  width: 182.w,
-                  height: 182.h,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.amber,
-                  ),
+              ),
+            ],
+          ),
+          Positioned(
+            top: 100.h, // Adjust for circle height
+            child: Container(
+              height: 182.w,
+              width: 182.w,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                shape: BoxShape.circle,
+                border: Border.all(color: Colors.white, width: 5),
+              ),
+              child: ClipOval(
+                child: Image.network(
+                  "snap.data.image", // Replace with your image URL
+                  fit: BoxFit.cover,
                 ),
-              ],
+              ),
             ),
           ),
         ],
