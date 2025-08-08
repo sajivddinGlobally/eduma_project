2f26af1f0929fc888e42ba631315e54092979403
sajivddin <sajivddin.websolutions@gmail.com>
2025-08-08 17:27:01 +0530

    change

--- lib/Screen/courseReview.page.dart
+++ lib/Screen/courseReview.page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CourseReviewPage extends StatefulWidget {
   const CourseReviewPage({super.key});
@@ -11,7 +12,19 @@ class _CourseReviewPageState extends State<CourseReviewPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 215.h,
+            decoration: BoxDecoration(color: Color(0xFF001E6C),
+            
+            
+            ),
+            child: Column(),),
+        ],
+      ),
     );
   }
 }
