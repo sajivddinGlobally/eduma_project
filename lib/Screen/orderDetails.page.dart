ae5b1db57bc5233a6380bfe407d8686361fb6f44
sajivddin <sajivddin.websolutions@gmail.com>
2025-08-11 13:05:13 +0530

    change

--- lib/Screen/orderDetails.page.dart
+++ lib/Screen/orderDetails.page.dart
@@ -37,52 +37,65 @@ class _OrderDetailsPageState extends State<OrderDetailsPage> {
               fit: BoxFit.fill,
             ),
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 33.h),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Container(
-                    width: 37.w,
-                    height: 37.h,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Color.fromARGB(25, 0, 0, 0),
-                    ),
-                    child: IconButton(
-                      style: IconButton.styleFrom(
-                        minimumSize: Size(0, 0),
-                        padding: EdgeInsets.zero,
-                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          Padding(
+            padding: EdgeInsets.only(left: 20.w, right: 20.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 33.h),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 37.w,
+                      height: 37.h,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Color.fromARGB(25, 0, 0, 0),
                       ),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      icon: Icon(
-                        Icons.arrow_back,
-                        color: Color(0xFF001E6C),
-                        size: 20.sp,
+                      child: IconButton(
+                        style: IconButton.styleFrom(
+                          minimumSize: Size(0, 0),
+                          padding: EdgeInsets.zero,
+                          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: Icon(
+                          Icons.arrow_back,
+                          color: Color(0xFF001E6C),
+                          size: 20.sp,
+                        ),
                       ),
                     ),
+                    SizedBox(width: 50.w),
+                    Center(
+                      child: Text(
+                        "Order Details",
+                        style: GoogleFonts.roboto(
+                          fontSize: 26.sp,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xFF1B1B1B),
+                          letterSpacing: -0.4,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 30.h),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10.r),
+                  child: Image.asset(
+                    "assets/shop1.png",
+                    width: MediaQuery.of(context).size.width,
+                    height: 198.h,
+                    fit: BoxFit.cover,
                   ),
-                  SizedBox(width: 50.w),
-                  Center(
-                    child: Text(
-                      "Order List",
-                      style: GoogleFonts.roboto(
-                        fontSize: 26.sp,
-                        fontWeight: FontWeight.w600,
-                        color: Color(0xFF1B1B1B),
-                        letterSpacing: -0.4,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
