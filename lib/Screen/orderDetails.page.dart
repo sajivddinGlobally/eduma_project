2e0bc6027356df799d4ec7db3eef3fba84a7f94c
sajivddin <sajivddin.websolutions@gmail.com>
2025-08-11 13:02:48 +0530

    change

--- lib/Screen/orderDetails.page.dart
+++ lib/Screen/orderDetails.page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class OrderDetailsPage extends StatefulWidget {
   const OrderDetailsPage({super.key});
@@ -11,7 +13,79 @@ class _OrderDetailsPageState extends State<OrderDetailsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Color(0xFFFFFFFF),
+      body: Stack(
+        children: [
+          Positioned(
+            left: -110,
+            top: -120.h,
+            child: Image.asset(
+              "assets/vect.png",
+              width: 363.w,
+              height: 270.h,
+              fit: BoxFit.fitHeight,
+            ),
+          ),
+          Positioned(
+            bottom: -40.h,
+            left: 0,
+            right: 0,
+            child: Image.asset(
+              "assets/vec.png",
+              width: 470.w,
+              height: 450.h,
+              fit: BoxFit.fill,
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 33.h),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 37.w,
+                    height: 37.h,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Color.fromARGB(25, 0, 0, 0),
+                    ),
+                    child: IconButton(
+                      style: IconButton.styleFrom(
+                        minimumSize: Size(0, 0),
+                        padding: EdgeInsets.zero,
+                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: Icon(
+                        Icons.arrow_back,
+                        color: Color(0xFF001E6C),
+                        size: 20.sp,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 50.w),
+                  Center(
+                    child: Text(
+                      "Order List",
+                      style: GoogleFonts.roboto(
+                        fontSize: 26.sp,
+                        fontWeight: FontWeight.w600,
+                        color: Color(0xFF1B1B1B),
+                        letterSpacing: -0.4,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
