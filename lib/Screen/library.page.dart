4238b29e1da990923550fac361787ccafe4a5cd5
sajivddin <sajivddin.websolutions@gmail.com>
2025-08-20 16:09:48 +0530

    chage

--- lib/Screen/library.page.dart
+++ lib/Screen/library.page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LibraryPage extends StatefulWidget {
   const LibraryPage({super.key});
@@ -11,7 +13,140 @@ class _LibraryPageState extends State<LibraryPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Color(0xFFFFFFFF),
+      body: Stack(
+        children: [
+          Positioned(
+            left: -120,
+            top: -100.h,
+            child: Image.asset(
+              "assets/vect.png",
+              width: 363.w,
+              height: 270.h,
+              fit: BoxFit.fitHeight,
+            ),
+          ),
+          Positioned(
+            bottom: -40.h,
+            left: 0,
+            right: 0,
+            child: Image.asset(
+              "assets/vec.png",
+              width: 470.w,
+              height: 450.h,
+              fit: BoxFit.fill,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 20.w, right: 20.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 33.h),
+                Row(
+                  children: [
+                    // Container(
+                    //   width: 37.w,
+                    //   height: 37.h,
+                    //   decoration: BoxDecoration(
+                    //     shape: BoxShape.circle,
+                    //     color: Color.fromARGB(25, 0, 0, 0),
+                    //   ),
+                    //   child: IconButton(
+                    //     style: IconButton.styleFrom(
+                    //       minimumSize: Size(0, 0),
+                    //       padding: EdgeInsets.zero,
+                    //       tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    //     ),
+                    //     onPressed: () {
+                    //       Navigator.pop(context);
+                    //     },
+                    //     icon: Icon(
+                    //       Icons.arrow_back,
+                    //       color: Color(0xFF001E6C),
+                    //       size: 20.sp,
+                    //     ),
+                    //   ),
+                    // ),
+                    //SizedBox(width: 50.w),
+                    Text(
+                      "Library",
+                      style: GoogleFonts.roboto(
+                        fontSize: 26.sp,
+                        fontWeight: FontWeight.w700,
+                        color: Color(0xFF1B1B1B),
+                        letterSpacing: -0.4,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 15.h),
+                Expanded(
+                  child: ListView.builder(
+                    padding: EdgeInsets.zero,
+                    itemCount: 2,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: EdgeInsets.only(top: 25.h),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(10.r),
+                                  child: Image.asset(
+                                    "assets/shop1.png",
+                                    width: 170.w,
+                                    height: 100.h,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                                SizedBox(width: 10.w),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    SizedBox(
+                                      width: 210.w,
+                                      // color: Colors.amber,
+                                      child: Text(
+                                        maxLines: 2,
+                                        overflow: TextOverflow.ellipsis,
+                                        "Sajivddin Ansari hellow hot are you sajiv",
+                                        style: GoogleFonts.roboto(
+                                          fontSize: 20.sp,
+                                          fontWeight: FontWeight.w500,
+                                          color: Colors.blue,
+                                          height: 1.5.h,
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(height: 10.h),
+                                    Text(
+                                      "data",
+                                      style: GoogleFonts.roboto(
+                                        fontSize: 16.sp,
+                                        fontWeight: FontWeight.w400,
+                                        color: Colors.blue,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
