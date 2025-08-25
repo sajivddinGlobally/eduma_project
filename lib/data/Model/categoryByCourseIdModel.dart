92c9f9c315204ae9ae90333dbd7b176a5ac85c9e
sajivddin <sajivddin.websolutions@gmail.com>
2025-08-25 12:25:19 +0530

    cjange

--- lib/data/Model/categoryByCourseIdModel.dart
+++ lib/data/Model/categoryByCourseIdModel.dart
@@ -1,127 +1,98 @@
-// To parse this JSON data, do
-//
-//     final corurseByCategoryIdModel = corurseByCategoryIdModelFromJson(jsonString);
-
 import 'dart:convert';
 
-CorurseByCategoryIdModel corurseByCategoryIdModelFromJson(String str) => CorurseByCategoryIdModel.fromJson(json.decode(str));
+CorurseByCategoryIdModel corurseByCategoryIdModelFromJson(String str) =>
+    CorurseByCategoryIdModel.fromJson(json.decode(str));
 
-String corurseByCategoryIdModelToJson(CorurseByCategoryIdModel data) => json.encode(data.toJson());
+String corurseByCategoryIdModelToJson(CorurseByCategoryIdModel data) =>
+    json.encode(data.toJson());
 
 class CorurseByCategoryIdModel {
-    Category category;
-    List<Course> courses;
-    int total;
-    int pages;
-    int currentPage;
+  Category category;
+  List<Course> courses;
+  int total;
+  int pages;
+  int currentPage;
 
-    CorurseByCategoryIdModel({
-        required this.category,
-        required this.courses,
-        required this.total,
-        required this.pages,
-        required this.currentPage,
-    });
+  CorurseByCategoryIdModel({
+    required this.category,
+    required this.courses,
+    required this.total,
+    required this.pages,
+    required this.currentPage,
+  });
 
-    factory CorurseByCategoryIdModel.fromJson(Map<String, dynamic> json) => CorurseByCategoryIdModel(
+  factory CorurseByCategoryIdModel.fromJson(Map<String, dynamic> json) =>
+      CorurseByCategoryIdModel(
         category: Category.fromJson(json["category"]),
-        courses: List<Course>.from(json["courses"].map((x) => Course.fromJson(x))),
-        total: json["total"],
-        pages: json["pages"],
-        currentPage: json["current_page"],
-    );
+        courses: List<Course>.from(
+          (json["courses"] ?? []).map((x) => Course.fromJson(x)),
+        ),
+        total: json["total"] ?? 0,
+        pages: json["pages"] ?? 0,
+        currentPage: json["current_page"] ?? 1,
+      );
 
-    Map<String, dynamic> toJson() => {
-        "category": category.toJson(),
-        "courses": List<dynamic>.from(courses.map((x) => x.toJson())),
-        "total": total,
-        "pages": pages,
-        "current_page": currentPage,
-    };
+  Map<String, dynamic> toJson() => {
+    "category": category.toJson(),
+    "courses": List<dynamic>.from(courses.map((x) => x.toJson())),
+    "total": total,
+    "pages": pages,
+    "current_page": currentPage,
+  };
 }
 
 class Category {
-    int id;
-    String name;
-    String slug;
+  int id;
+  String name;
+  String slug;
 
-    Category({
-        required this.id,
-        required this.name,
-        required this.slug,
-    });
+  Category({required this.id, required this.name, required this.slug});
 
-    factory Category.fromJson(Map<String, dynamic> json) => Category(
-        id: json["id"],
-        name: json["name"],
-        slug: json["slug"],
-    );
+  factory Category.fromJson(Map<String, dynamic> json) => Category(
+    id: json["id"] ?? 0,
+    name: json["name"] ?? '',
+    slug: json["slug"] ?? '',
+  );
 
-    Map<String, dynamic> toJson() => {
-        "id": id,
-        "name": name,
-        "slug": slug,
-    };
+  Map<String, dynamic> toJson() => {"id": id, "name": name, "slug": slug};
 }
 
 class Course {
-    int id;
-    String title;
-    String excerpt;
-    String thumbnail;
-    String link;
-    Price price;
-    dynamic enrolled;
+  int id;
+  String title;
+  String excerpt;
+  String thumbnail;
+  String link;
+  String price; // enum हटाकर string कर दिया
+  String? enrolled; // nullable
 
-    Course({
-        required this.id,
-        required this.title,
-        required this.excerpt,
-        required this.thumbnail,
-        required this.link,
-        required this.price,
-        required this.enrolled,
-    });
+  Course({
+    required this.id,
+    required this.title,
+    required this.excerpt,
+    required this.thumbnail,
+    required this.link,
+    required this.price,
+    this.enrolled,
+  });
 
-    factory Course.fromJson(Map<String, dynamic> json) => Course(
-        id: json["id"],
-        title: json["title"],
-        excerpt: json["excerpt"],
-        thumbnail: json["thumbnail"],
-        link: json["link"],
-        price: priceValues.map[json["price"]]!,
-        enrolled: json["enrolled"],
-    );
+  factory Course.fromJson(Map<String, dynamic> json) => Course(
+    id: json["id"] ?? 0,
+    title: json["title"] ?? '',
+    excerpt: json["excerpt"] ?? '',
+    thumbnail: json["thumbnail"] ?? '',
+    link: json["link"] ?? '',
+    price: json["price"] ?? 'Free', // default value दिया
+    enrolled: json["enrolled"],
+  );
 
-    Map<String, dynamic> toJson() => {
-        "id": id,
-        "title": title,
-        "excerpt": excerpt,
-        "thumbnail": thumbnail,
-        "link": link,
-        "price": priceValues.reverse[price],
-        "enrolled": enrolled,
-    };
-}
-
-enum Price {
-    THE_100000,
-    THE_52500
-}
-
-final priceValues = EnumValues({
-    "₹1,000.00": Price.THE_100000,
-    "₹525.00": Price.THE_52500
-});
-
-class EnumValues<T> {
-    Map<String, T> map;
-    late Map<T, String> reverseMap;
-
-    EnumValues(this.map);
-
-    Map<T, String> get reverse {
-            reverseMap = map.map((k, v) => MapEntry(v, k));
-            return reverseMap;
-    }
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "title": title,
+    "excerpt": excerpt,
+    "thumbnail": thumbnail,
+    "link": link,
+    "price": price,
+    "enrolled": enrolled,
+  };
 }
