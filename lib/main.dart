0211795aa7f1d85eaa6a44845a200a6393ae0a97
sajivddin <sajivddin.websolutions@gmail.com>
2025-08-27 16:54:26 +0530

    change

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,6 @@ void main() async {
 class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final themeMode = ref.watch(themeNotifierProvider);
