6488a06286068a8c39335d772c4d2285e3e3eb48
sajivddin <sajivddin.websolutions@gmail.com>
2025-09-01 16:33:16 +0530

    change

--- lib/Screen/cart.page.dart
+++ lib/Screen/cart.page.dart
@@ -660,34 +660,30 @@ class _CartBodyState extends ConsumerState<CartBody> {
   Widget build(BuildContext context) {
     final totalPrice = widget.data.price * widget.data.quantity;
 
-    return InkWell(
-      onTap: () {
-        Navigator.push(
-          context,
-          CupertinoPageRoute(
-            builder: (context) =>
-                ProductDetailsPage(id: widget.data.productId.toString()),
-          ),
-        );
-      },
-      child: Container(
-        margin: EdgeInsets.only(top: 15.h),
-        padding: EdgeInsets.all(12.w),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16.r),
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black12,
-              blurRadius: 6,
-              offset: Offset(0, 2),
-            ),
-          ],
-        ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ClipRRect(
+    return Container(
+      margin: EdgeInsets.only(top: 15.h),
+      padding: EdgeInsets.all(12.w),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16.r),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(color: Colors.black12, blurRadius: 6, offset: Offset(0, 2)),
+        ],
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                CupertinoPageRoute(
+                  builder: (context) =>
+                      ProductDetailsPage(id: widget.data.productId.toString()),
+                ),
+              );
+            },
+            child: ClipRRect(
               borderRadius: BorderRadius.circular(12.r),
               child: Image.network(
                 widget.data.thumbnail,
@@ -708,157 +704,149 @@ class _CartBodyState extends ConsumerState<CartBody> {
                 },
               ),
             ),
-            SizedBox(width: 12.w),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    widget.data.name,
-                    style: GoogleFonts.poppins(
-                      fontSize: 16.sp,
-                      fontWeight: FontWeight.w600,
-                      color: Color(0xFF001E6C),
-                    ),
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
+          ),
+          SizedBox(width: 12.w),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.data.name,
+                  style: GoogleFonts.poppins(
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xFF001E6C),
                   ),
-                  SizedBox(height: 8.h),
-                  Text(
-                    "₹${widget.data.price}",
-                    style: GoogleFonts.poppins(
-                      fontSize: 14.sp,
-                      fontWeight: FontWeight.w500,
-                      color: Color(0xFF001E6C),
-                    ),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                SizedBox(height: 8.h),
+                Text(
+                  "₹${widget.data.price}",
+                  style: GoogleFonts.poppins(
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xFF001E6C),
                   ),
-                  SizedBox(height: 8.h),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Qty: ${widget.data.quantity}",
-                        style: GoogleFonts.poppins(
-                          fontSize: 14.sp,
-                          fontWeight: FontWeight.w500,
-                          color: Color(0xFF747474),
-                        ),
+                ),
+                SizedBox(height: 8.h),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Qty: ${widget.data.quantity}",
+                      style: GoogleFonts.poppins(
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w500,
+                        color: Color(0xFF747474),
                       ),
-                      Container(
-                        width: 120.w,
-                        height: 36.h,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10.r),
-                          color: Color(0xFFF5F7FA),
-                        ),
-                        child: Padding(
-                          padding: EdgeInsets.only(left: 10.w, right: 10.w),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              IconButton(
-                                style: IconButton.styleFrom(
-                                  minimumSize: Size.zero,
-                                  padding: EdgeInsets.zero,
-                                  tapTargetSize:
-                                      MaterialTapTargetSize.shrinkWrap,
-                                ),
-                                icon: Icon(
-                                  Icons.remove,
-                                  size: 20.sp,
-                                  color: Color(0xFF001E6C),
-                                ),
-                                onPressed: isUpdating
-                                    ? null
-                                    : () async {
-                                        await removeCartQuantity(
-                                          widget.data.productId,
-                                        );
-                                      },
-                              ),
-                              Text(
-                                "${widget.data.quantity}",
-                                style: GoogleFonts.poppins(
-                                  fontSize: 16.sp,
-                                  fontWeight: FontWeight.w600,
-                                  color: Color(0xFF001E6C),
-                                ),
-                              ),
-                              IconButton(
-                                style: IconButton.styleFrom(
-                                  minimumSize: Size.zero,
-                                  padding: EdgeInsets.zero,
-                                  tapTargetSize:
-                                      MaterialTapTargetSize.shrinkWrap,
-                                ),
-                                icon: Icon(
-                                  Icons.add,
-                                  size: 20.sp,
-                                  color: Color(0xFF001E6C),
-                                ),
-                                onPressed: isUpdating
-                                    ? null
-                                    : () async {
-                                        await updateCartQuantity(
-                                          widget.data.productId,
-                                          1,
-                                        );
-                                      },
-                              ),
-                            ],
+                    ),
+                    Spacer(),
+                    Container(
+                      // width: 120.w,
+                      height: 36.h,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.r),
+                        color: Color(0xFFF5F7FA),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          IconButton(
+                            style: IconButton.styleFrom(
+                              padding: EdgeInsets.zero,
+                              minimumSize: Size.square(25),
+                            ),
+                            icon: Icon(
+                              Icons.remove,
+                              size: 20.sp,
+                              color: Color(0xFF001E6C),
+                            ),
+                            onPressed: isUpdating
+                                ? null
+                                : () async {
+                                    await removeCartQuantity(
+                                      widget.data.productId,
+                                    );
+                                  },
                           ),
-                        ),
+                          Text(
+                            "${widget.data.quantity}",
+                            style: GoogleFonts.poppins(
+                              fontSize: 16.sp,
+                              fontWeight: FontWeight.w600,
+                              color: Color(0xFF001E6C),
+                            ),
+                          ),
+                          IconButton(
+                            style: IconButton.styleFrom(
+                              padding: EdgeInsets.zero,
+                              minimumSize: Size.square(25),
+                            ),
+                            icon: Icon(
+                              Icons.add,
+                              size: 20.sp,
+                              color: Color(0xFF001E6C),
+                            ),
+                            onPressed: isUpdating
+                                ? null
+                                : () async {
+                                    await updateCartQuantity(
+                                      widget.data.productId,
+                                      1,
+                                    );
+                                  },
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            InkWell(
-              onTap: () async {
-                final body = CarRemoveBodyModel(
-                  productId: widget.data.productId,
-                );
-                try {
-                  final service = APIStateNetwork(createDio());
-                  final response = await service.removeCart(body);
-                  if (response.success == true) {
-                    showSuccessMessage(context, response.message);
-                    ref.invalidate(cartController);
-                  } else {
-                    showSuccessMessage(
-                      context,
-                      "Failed to remove item: ${response.message}",
-                    );
-                  }
-                } catch (e) {
-                  log(e.toString());
-                  showSuccessMessage(context, "Error removing item: $e");
-                }
-              },
-              child: Container(
-                width: 40.w,
-                height: 40.h,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black12,
-                      blurRadius: 6.r,
-                      offset: Offset(0, 2),
                     ),
                   ],
                 ),
-                child: Icon(
-                  Icons.delete_outline,
-                  color: Colors.redAccent,
-                  size: 24.sp,
-                ),
-              ),
+              ],
             ),
-          ],
-        ),
+          ),
+          // InkWell(
+          //   onTap: () async {
+          //     final body = CarRemoveBodyModel(productId: widget.data.productId);
+          //     try {
+          //       final service = APIStateNetwork(createDio());
+          //       final response = await service.removeCart(body);
+          //       if (response.success == true) {
+          //         showSuccessMessage(context, response.message);
+          //         ref.invalidate(cartController);
+          //       } else {
+          //         showSuccessMessage(
+          //           context,
+          //           "Failed to remove item: ${response.message}",
+          //         );
+          //       }
+          //     } catch (e) {
+          //       log(e.toString());
+          //       showSuccessMessage(context, "Error removing item: $e");
+          //     }
+          //   },
+          //   child: Container(
+          //     width: 40.w,
+          //     height: 40.h,
+          //     decoration: BoxDecoration(
+          //       shape: BoxShape.circle,
+          //       color: Colors.white,
+          //       boxShadow: [
+          //         BoxShadow(
+          //           color: Colors.black12,
+          //           blurRadius: 6.r,
+          //           offset: Offset(0, 2),
+          //         ),
+          //       ],
+          //     ),
+          //     child: Icon(
+          //       Icons.delete_outline,
+          //       color: Colors.redAccent,
+          //       size: 24.sp,
+          //     ),
+          //   ),
+          // ),
+        ],
       ),
     );
   }
