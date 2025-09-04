31ba48303e9ac88e7dc3bfe65a5c87178f3c8aac
sajivddin <sajivddin.websolutions@gmail.com>
2025-09-04 12:58:46 +0530

    change

--- lib/Screen/video.page.dart
+++ lib/Screen/video.page.dart
@@ -1,3 +1,121 @@
+///////// ye video playser se hai pacage
+///
+// import 'package:flutter/material.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
+// import 'package:video_player/video_player.dart';
+
+// class VideoPge extends StatefulWidget {
+//   final String videoId; // ✅ Normal video file ka URL ya asset path
+
+//   const VideoPge({super.key, required this.videoId});
+
+//   @override
+//   State<VideoPge> createState() => _VideoPgeState();
+// }
+
+// class _VideoPgeState extends State<VideoPge> {
+//   late VideoPlayerController _controller;
+//   bool _isLoading = true;
+
+//   @override
+//   void initState() {
+//     super.initState();
+
+//     // ✅ Network video ke liye
+//     _controller = VideoPlayerController.networkUrl(Uri.parse(widget.videoId))
+//       ..initialize().then((_) {
+//         setState(() {
+//           _isLoading = false;
+//         });
+//         _controller.play(); // auto play
+//       });
+//   }
+
+//   @override
+//   void dispose() {
+//     _controller.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: const Color(0xFFFFFFFF),
+//       body: Stack(
+//         children: [
+//           // Background decoration
+//           Positioned(
+//             left: -120,
+//             top: -100.h,
+//             child: Image.asset(
+//               "assets/vect.png",
+//               width: 363.w,
+//               height: 270.h,
+//               fit: BoxFit.fitHeight,
+//             ),
+//           ),
+//           Positioned(
+//             bottom: -40.h,
+//             left: 0,
+//             right: 0,
+//             child: Image.asset(
+//               "assets/vec.png",
+//               width: 470.w,
+//               height: 450.h,
+//               fit: BoxFit.fill,
+//             ),
+//           ),
+
+//           // Main content
+//           Column(
+//             children: [
+//               SizedBox(height: 40.h),
+//               Expanded(
+//                 child: Center(
+//                   child: _isLoading
+//                       ? const CircularProgressIndicator()
+//                       : AspectRatio(
+//                           aspectRatio: _controller.value.aspectRatio,
+//                           child: Stack(
+//                             alignment: Alignment.bottomCenter,
+//                             children: [
+//                               VideoPlayer(_controller),
+//                               VideoProgressIndicator(
+//                                 _controller,
+//                                 allowScrubbing: true,
+//                                 padding: const EdgeInsets.all(8),
+//                               ),
+//                               IconButton(
+//                                 icon: Icon(
+//                                   _controller.value.isPlaying
+//                                       ? Icons.pause
+//                                       : Icons.play_arrow,
+//                                   size: 40,
+//                                   color: Colors.white,
+//                                 ),
+//                                 onPressed: () {
+//                                   setState(() {
+//                                     _controller.value.isPlaying
+//                                         ? _controller.pause()
+//                                         : _controller.play();
+//                                   });
+//                                 },
+//                               ),
+//                             ],
+//                           ),
+//                         ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// ///// ye dusra package hai ok
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
@@ -13,6 +131,7 @@ class VideoPge extends StatefulWidget {
 
 class _VideoPgeState extends State<VideoPge> {
   late YoutubePlayerController _controller;
+  bool _isLoading = true;
 
   @override
   void initState() {
@@ -26,6 +145,15 @@ class _VideoPgeState extends State<VideoPge> {
         controlsVisibleAtStart: true,
       ),
     );
+
+    // simulate loading time for smoother UX
+    Future.delayed(const Duration(seconds: 2), () {
+      if (mounted) {
+        setState(() {
+          _isLoading = false;
+        });
+      }
+    });
   }
 
   @override
@@ -37,42 +165,19 @@ class _VideoPgeState extends State<VideoPge> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFFFFFFF),
-      body: Stack(
-        children: [
-          Positioned(
-            left: -120,
-            top: -100.h,
-            child: Image.asset(
-              "assets/vect.png",
-              width: 363.w,
-              height: 270.h,
-              fit: BoxFit.fitHeight,
-            ),
-          ),
-          Positioned(
-            bottom: -40.h,
-            left: 0,
-            right: 0,
-            child: Image.asset(
-              "assets/vec.png",
-              width: 470.w,
-              height: 450.h,              fit: BoxFit.fill,
-            ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 0.h),
-              YoutubePlayer(
+      backgroundColor: const Color(0xFFFFFFFF),
+      body: _isLoading
+          ? const CircularProgressIndicator()
+          : YoutubePlayerBuilder(
+              player: YoutubePlayer(
                 controller: _controller,
                 showVideoProgressIndicator: true,
                 progressIndicatorColor: Colors.red,
               ),
-            ],
-          ),
-        ],
-      ),
+              builder: (context, player) {
+                return AspectRatio(aspectRatio: 16 / 9, child: player);
+              },
+            ),
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,6 +11,7 @@ import file_selector_macos
 import flutter_inappwebview_macos
 import path_provider_foundation
 import url_launcher_macos
+import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
@@ -19,4 +20,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
