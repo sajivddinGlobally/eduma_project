f94a5e8294909998dd9e06de749dde6f9ba7420f
sajivddin <sajivddin.websolutions@gmail.com>
2025-09-06 16:25:10 +0530

    cage

--- lib/Screen/video.page.dart
+++ lib/Screen/video.page.dart
@@ -145,9 +145,9 @@ class _VideoPageState extends State<VideoPage> {
     });
   }
 
-   Future<void> _updateLessonProgress(String videoId) async {
+  Future<void> _updateLessonProgress(String videoId) async {
     final box = await Hive.openBox('userBox');
-    await box.put('lesson_$videoId', true); 
+    await box.put('lesson_$videoId', true);
   }
 
   @override
@@ -179,3 +179,133 @@ class _VideoPageState extends State<VideoPage> {
     );
   }
 }
+
+//////////////////// without cheiwe se only video player se
+///
+
+// import 'dart:developer';
+// import 'package:flutter/material.dart';
+// import 'package:video_player/video_player.dart';
+// import 'package:youtube_explode_dart/youtube_explode_dart.dart';
+
+// class VideoPage extends StatefulWidget {
+//   final String videoId;
+
+//   const VideoPage({super.key, required this.videoId});
+
+//   @override
+//   State<VideoPage> createState() => _VideoPageState();
+// }
+
+// class _VideoPageState extends State<VideoPage> {
+//   late VideoPlayerController _videoPlayerController;
+//   bool _isLoading = true;
+//   final YoutubeExplode _yt = YoutubeExplode();
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _loadVideo();
+//   }
+
+//   Future<void> _loadVideo() async {
+//     try {
+//       var manifest = await _yt.videos.streamsClient.getManifest(widget.videoId);
+
+//       var muxedStreams = manifest.muxed.toList();
+
+//       if (muxedStreams.isEmpty) {
+//         // fallback to audio-only
+//         var audioStream = manifest.audioOnly.isNotEmpty
+//             ? manifest.audioOnly.first
+//             : null;
+//         if (audioStream == null) throw Exception("No playable streams found!");
+//         _videoPlayerController = VideoPlayerController.networkUrl(
+//           Uri.parse(audioStream.url.toString()),
+//         );
+//       } else {
+//         // 720p try kare, nahi to last element (highest resolution)
+//         MuxedStreamInfo streamInfo = muxedStreams.firstWhere(
+//           (s) => s.videoResolution.height == 720,
+//           orElse: () => muxedStreams.last, // highest resolution stream
+//         );
+
+//         _videoPlayerController = VideoPlayerController.networkUrl(
+//           Uri.parse(streamInfo.url.toString()),
+//         );
+//       }
+
+//       await _videoPlayerController.initialize();
+//       _videoPlayerController.play();
+
+//       setState(() {
+//         _isLoading = false;
+//       });
+//     } catch (e) {
+//       log("Error loading video: $e");
+//     }
+//   }
+
+//   @override
+//   void dispose() {
+//     _videoPlayerController.dispose();
+//     _yt.close();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Stack(
+//         children: [
+//           _isLoading
+//               ? const CircularProgressIndicator()
+//               : AspectRatio(
+//                   aspectRatio: _videoPlayerController.value.aspectRatio,
+//                   child: Stack(
+//                     alignment: Alignment.bottomCenter,
+//                     children: [
+//                       VideoPlayer(_videoPlayerController),
+
+//                       // Play/Pause Button
+//                       Align(
+//                         alignment: Alignment.center,
+//                         child: IconButton(
+//                           iconSize: 60,
+//                           color: Colors.white,
+//                           icon: Icon(
+//                             _videoPlayerController.value.isPlaying
+//                                 ? Icons.pause_circle
+//                                 : Icons.play_circle,
+//                           ),
+//                           onPressed: () {
+//                             setState(() {
+//                               if (_videoPlayerController.value.isPlaying) {
+//                                 _videoPlayerController.pause();
+//                               } else {
+//                                 _videoPlayerController.play();
+//                               }
+//                             });
+//                           },
+//                         ),
+//                       ),
+
+//                       // Progress Bar
+//                       VideoProgressIndicator(
+//                         _videoPlayerController,
+//                         allowScrubbing: true,
+//                         padding: const EdgeInsets.all(8),
+//                         colors: VideoProgressColors(
+//                           playedColor: Colors.red,
+//                           bufferedColor: Colors.grey,
+//                           backgroundColor: Colors.black26,
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//         ],
+//       ),
+//     );
+//   }
+// }
