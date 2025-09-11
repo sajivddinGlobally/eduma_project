782bd170b73eea75f468068f919e6404d8433f8f
sajivddin <sajivddin.websolutions@gmail.com>
2025-09-11 13:19:17 +0530

    change

--- lib/data/Controller/allCoursesController.dart
+++ lib/data/Controller/allCoursesController.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:eduma_app/config/network/api.state.dart';
 import 'package:eduma_app/config/utils/pretty.dio.dart';
 import 'package:eduma_app/data/Model/allCoursesModel.dart';
@@ -8,9 +10,21 @@ final allCoursesController = FutureProvider<AllCoursesModel>((ref) async {
   return await allCourseService.allCourses();
 });
 
-
-
-
-
 class PopularCouserNotifier extends StateNotifier<AsyncValue<AllCoursesModel>> {
-  
+  PopularCouserNotifier() : super(AsyncValue.loading());
+
+  int page = 1;
+  final int limit = 10;
+  bool hasMore = true;
+  bool isLoading = false;
+
+  Future<void> loadMoreCourse({bool refresh = false}) async {
+    if (refresh) {
+      page = 1;
+      hasMore = true;
+      state = AsyncValue.loading();
+    }
+    isLoading = true;
+    log("API call start : page = $page, limit= $limit");
+  }
+}
