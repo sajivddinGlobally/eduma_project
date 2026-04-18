d095b552ef6b96bcb2662e68f5a5dbe1f50a3e06
sajivddin <sajivddin.websolutions@gmail.com>
2026-04-18 10:57:56 +0530

    change

--- lib/Screen/home.page.dart
+++ lib/Screen/home.page.dart
@@ -100,6 +100,7 @@ class _HomePageState extends ConsumerState<HomePage> {
     //     ),
     //   );
     // }
+
     return WillPopScope(
       onWillPop: () async {
         if (selectIndex != 0) {
